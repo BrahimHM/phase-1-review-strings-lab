2f0df667ca93e27ae3406f56ae3217804db8a917
Brahim HM <brahimhm98@gmail.com>
2023-07-16 18:57:54 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser="Brahim";
+let welcomeMessage=`hi ${currentUser} Welcome to Flatbook, !`;
+let excitedWelcomeMessage=welcomeMessage.toUpperCase();
+let shortGreeting=`Welcome, ${currentUser.slice(0, 1)}!`;
